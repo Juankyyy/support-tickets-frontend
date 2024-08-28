bdb275fc7113a4f62185c334286e2090577093ef
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-08-28 02:21:04 -0500

    feat: Login Page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="content">
-        
+        <form >
+            <router-link to="/" class="btn btn-ghost btn-active text-2xl mb-3">Support Tickets</router-link>
+            <label class="input sm:w-96 input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70"> <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /> <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                <input type="text" class="grow" placeholder="Email" />
+            </label>
+    
+            <label class="input sm:w-96 input-bordered flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70"> <path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
+                <input type="password" class="grow" placeholder="123456"/>
+            </label>
+
+            <button class="btn btn-success w-full font-bold">Iniciar Sesión</button>
+        </form>
     </div>
 </template>
 
@@ -14,10 +27,14 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    height: 100dvh;
 }
 
 form {
-    width: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
 }
 </style>
