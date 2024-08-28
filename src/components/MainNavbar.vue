39320a0338b715126206fe035cd5d0fd14becaad
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-08-28 01:49:14 -0500

    feat: Navbar Responsive, Route Active
    Se hace el responsive del Navbar y dependiendo de la página el que esté el botón estará activo o no

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -2,22 +2,22 @@
     <div class="navbar bg-base-100">
         <div class="navbar-start">
             <div class="dropdown">
-                <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <div tabindex="0" role="button" class="btn btn-ghost md:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
                 <ul tabindex="0"
                     class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                     
-                    <li><a>Mis Tickets</a></li>
+                    <li><router-link to="/" :class="{ active: $route.path === '/' }" class="mb-1">Mis Tickets</router-link></li>
                     <li><a>Item 3</a></li>
                 </ul>
             </div>
 
-            <router-link to="/" class="btn btn-ghost text-xl">Support Tickets</router-link>
+            <router-link to="/" class="btn btn-ghost text-sm md:text-xl">Support Tickets</router-link>
             
-            <div class="hidden lg:flex">
+            <div class="hidden md:flex">
                 <ul class="menu menu-horizontal px-1">
-                    <li><router-link to="/">Mis Tickets</router-link></li>
+                    <li><router-link to="/" :class="{ active: $route.path === '/' }" class="mr-1">Mis Tickets</router-link></li>
                     <li><a>Item 3</a></li>
                 </ul>
             </div>
@@ -25,9 +25,9 @@
 
 
         <div class="navbar-end">
-            <router-link to="/" class="btn btn-success mr-5">Crear nuevo Ticket</router-link>
+            <router-link to="/" class="btn btn-success mr-5 btn-sm sm:btn-md">Crear nuevo Ticket</router-link>
             
-            <router-link to="/login" class="btn btn-error">
+            <router-link to="/login" class="btn btn-error btn-sm sm:btn-md">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 14"><path fill="#1d222b" fill-rule="evenodd" d="M0 1.5A1.5 1.5 0 0 1 1.5 0h7A1.5 1.5 0 0 1 10 1.5v1.939a2 2 0 0 0-.734 1.311H5.75a2.25 2.25 0 1 0 0 4.5h3.516A2 2 0 0 0 10 10.561V12.5A1.5 1.5 0 0 1 8.5 14h-7A1.5 1.5 0 0 1 0 12.5zm10.963 2.807A.75.75 0 0 0 10.5 5v1H5.75a1 1 0 0 0 0 2h4.75v1a.75.75 0 0 0 1.28.53l2-2a.75.75 0 0 0 0-1.06l-2-2a.75.75 0 0 0-.817-.163" clip-rule="evenodd"/></svg>
             </router-link>
         </div>
