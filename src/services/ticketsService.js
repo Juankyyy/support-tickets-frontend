97c74132ea71db2732464b250003f4b9a49efb23
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-09-06 01:06:56 -0500

    feat: NotyF Server

--- src/services/ticketsService.js
+++ src/services/ticketsService.js
@@ -1,3 +1,13 @@
+// Certificado SSL en somee hasta el 2/12/2024.
+
+import { Notyf } from "notyf";
+import 'notyf/notyf.min.css';
+
+// const notyf = new Notyf();
+const notyfServer = new Notyf({
+    duration: 30000
+});
+
 const ticketService = {
     // Tickets del usuario
     async GetMyTickets() {
@@ -20,6 +30,7 @@ const ticketService = {
                 return { ok: true, data: d }
             }
         } catch (err) {
+            notyfServer.error('Hubo un error en el servidor');
             console.error(`Error fetch tickets: ${err}`);
         }
     },
@@ -54,6 +65,7 @@ const ticketService = {
                 return { ok: true, data: d }
             }
         } catch (err) {
+            notyfServer.error('Hubo un error en el servidor');
             console.error(`Error fetch crear tickets: ${err}`);
         }
     }
